df1d46dcdaa594bf41842bf8962dbb22425a95cd
Gene <38320664+123wg@users.noreply.github.com>
2026-02-04 22:53:38 +0800

    chore(types): add common numeric tuples

--- packages/math/src/types/type_guard.ts
+++ packages/math/src/types/type_guard.ts
@@ -6,9 +6,32 @@
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export type Ctor = abstract new (...args: any[]) => unknown
 
+/** 2D 向量元组 */
+export type Num2 = readonly [number, number]
+
+/** 3D 向量元组 */
+export type Num3 = readonly [number, number, number]
+
+/** 4D 向量元组 */
+export type Num4 = readonly [number, number, number, number]
+
+/** 2x2 矩阵行主序数组 */
+export type Num2x2 = readonly [
+    number, number,
+    number, number,
+]
+
 /** 3x3 矩阵行主序数组 */
 export type Num3x3 = readonly [
     number, number, number,
     number, number, number,
     number, number, number,
+]
+
+/** 4x4 矩阵行主序数组 */
+export type Num4x4 = readonly [
+    number, number, number, number,
+    number, number, number, number,
+    number, number, number, number,
+    number, number, number, number,
 ]
