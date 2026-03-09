68c8d268f3014998b8bac6872dbdeccb1ab8ec63
wg <38320664+123wg@users.noreply.github.com>
2026-03-09 11:33:12 +0800

    chore(platform): wire workspace dependencies and canvas exports

--- packages/canvas/src/index.ts
+++ packages/canvas/src/index.ts
@@ -1,1 +1,23 @@
-export * from './types/type_define'
+export {
+    EN_NativeMouseEvent,
+    EN_NativeKeyboardEvent,
+    EN_MouseEvent,
+    EN_KeyboardEvent,
+    type IMouseEvent,
+    type IKeyboardEvent,
+} from './types/type_define'
+export { type ICCanvas } from './canvas/i_ccanvas'
+export { DefaultController } from './controller/default_controller'
+export { FnKey } from './controller/fn_key'
+export {
+    type IProcessKeyboardEvent,
+    type IKeyboardController,
+} from './controller/i_keyboard_controller'
+export {
+    type IProcessMouseEvent,
+    type IMouseController,
+} from './controller/i_mouse_controller'
+export { type IProcessEvent } from './controller/i_process_event'
+export { KeyboardInteractor } from './controller/keyboard_interactor'
+export { MouseInteractor } from './controller/mouse_interactor'
+export { canvasConfig } from './toolkit/canvas_config'
