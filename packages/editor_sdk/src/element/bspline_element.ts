7f8833f7a0cd1fabff9adb9a10cefab39564eeca
wg <38320664+123wg@users.noreply.github.com>
2026-03-17 17:02:58 +0800

    refactor(editor_sdk): adjust curve element defaults

--- packages/editor_sdk/src/element/bspline_element.ts
+++ packages/editor_sdk/src/element/bspline_element.ts
@@ -4,20 +4,20 @@ import { CurveElement } from './curve_element';
 
 @RegisterElement('6b26d106-4962-4440-a21c-b4fa7b1f64e6')
 export class BSplineElement extends CurveElement<NurbsCurve2> {
-    public controlPoints: Vec2[] = []
+    public controlPoints: Vec2[] = [new Vec2()]
 
     public degree = 3
 
-    public knots?: number[]
+    public knots: number[] = []
 
-    public weights?: number[]
+    public weights: number[] = []
 
     protected _updateCurve() {
         this.C_Curve = NurbsCurve2.makeByControlPoints(
             this.controlPoints,
             this.degree,
-            this.knots,
-            this.weights,
+            this.knots.length ? this.knots : undefined,
+            this.weights.length ? this.weights : undefined,
         )
     }
 }
--- packages/editor_sdk/src/element/line_element.ts
+++ packages/editor_sdk/src/element/line_element.ts
@@ -6,7 +6,7 @@ import { CurveElement } from './curve_element';
 export class LineElement extends CurveElement<Ln2> {
     public start: Vec2 = new Vec2()
 
-    public end: Vec2 = new Vec2()
+    public end: Vec2 = new Vec2(1, 0)
 
     protected _updateCurve() {
         this.C_Curve = new Ln2(this.start, this.end)
